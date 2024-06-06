7d7c523ee80c63629d47bf471a90f21bdc72d29e
ankitathakur-developer <at9008591@gmail.com>
2024-06-06 12:09:43 +0530

    Adding files to main
    Adding files to main

---
+++ index.php
@@ -0,0 +1,96 @@
+<?php 
+get_header("home");
+?>
+                <table class="email" style="margin: 0 auto ; font-family: Poppins, sans-serif ;  border-collapse: separate;
+                border-spacing: 0px; background-color: #fff !important;">
+                    <tbody>
+                        <tr>
+                            <td>
+    <div>
+    </div>
+                        
+</td>
+                    </tr>				
+                    <tr>
+                        <td class="logo-td" style="">
+                            <a href="https://nolomiami.com/"><img src="https://i.ibb.co/hc8Lsgp/image-logo.jpg" alt="image-logo" border="0"></a>
+                        </td>
+                        <td class="name" style=" height: 100%;
+                        vertical-align: top;">
+                            <table class="name-designation"  align="top">
+                                <tbody>
+                                    <tr>
+                                        <td style="">                                            
+                                                <img src="https://i.ibb.co/n691Tb9/signature-1.png" alt="signature-1" border="0">                                          
+                                        </td>                                        
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <p class="designation"  style=" font-size: 0.98rem !important;
+                                            line-height: 19px; font-weight: 400; letter-spacing: 3.6px; margin: 0px; text-transform: uppercase;  
+font-style: italic; text-align: right;">
+                                                Co-owner & founder
+                                            </p>
+                                        </td>                               
+                                    </tr> 
+                                </tbody>     
+                            </table>
+                            <table class="contact-details" style="width: 100%; text-align: right !important; margin-top:90px; text-align: right;">
+                                <tbody>
+                                    
+                                    <tr>
+                                        <td>
+                                            <a class="mobile" href="tel:+13059046797" style=" text-decoration: none;
+                                            font-weight: 300;
+                                            color: #000 !important; font-size: 1.2rem;
+                                            line-height: 21px;">
+                                                +305-904-6797
+                                            </a>
+                                        </td>
+                                       
+                                    </tr>
+                                    <tr>
+                                        <td style="font-size: 1.2rem;  line-height: 21px; font-weight: 300;
+                                        color: #000 !important; 
+                                       ">
+                                  
+                                      <a class="mail" href="mailto:karen@nolomiami.com" style="font-size: 1.2rem;
+                    line-height: 21px; color: #000 !important; text-decoration: none; text-decoration: none;">
+                                                karen@nolomiami.com
+                                            </a>
+                                  
+
+                                            
+                                        </td>   
+                                    </tr> 
+                                    <tr>
+                                        <td>
+                                            <table class="globe-main"  style="width: 100% !important; border-spacing: 0px !important;">
+                                                <tbody>
+                                                    <td style="padding-top: 4px;">
+                                                        <img src="https://i.ibb.co/wpy4BtY/globe.png" alt="globe" border="0">
+                                                    </td>
+                                                    <td style="width: 100%; text-align: right !important; font-weight: 300;
+                                                    color: #000 !important;  font-size: 1.2rem !important;
+                                                    line-height: 21px; ">
+
+                                                        <a class="website" href="https://nolomiami.com" style="font-size: 1.2rem !important;
+                    line-height: 21px; text-decoration: none !important; color: #000 !important; text-align: right !important;">
+                                                           nolomiami.com
+                                                        </a>
+                                                    </td>
+                                                </tbody>
+                                            </table>
+                                        </td>
+                                    </tr>     
+                                </tbody>     
+                            </table>                           
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+           
+
+
+                    </body>
+    </html>
